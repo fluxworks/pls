9965f471b0cbadd7b7c42f88d4da8a019a3a7bff
The Operator <mrjustinlucas@gmail.com>
2026-09-10 20:01:32 -0400

    26.09.10.2000

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,39 @@
     dead_code,
     unknown_lints,
     unused_imports,
+    unused_unsafe,
+    unused_variables,
 )]
+
+extern crate regex as re;
 
 #[macro_use] pub mod macros
 {
 
+}
+
+pub mod api
+{
+    use crate::
+    {
+        *,
+    };
+
+    pub const API:[&str; 20] =  [ "alias", "bg", "cd", "check", "cinfo", "exec", "exit", "export", "fg", "history", "jobs", "read", "source", "ulimit", "unalias", "vox", "minfd", "set", "unset", "unpath", ];
+}
+
+pub mod collections
+{
+    pub use std::collections::{ * };
+}
+
+pub mod env
+{
+    pub use std::env::{ * };
+    use crate::
+    {
+        *,
+    };
 }
 
 pub mod hash
@@ -16,10 +44,57 @@ pub mod hash
     pub use std::hash::{ * };
 }
 
+pub mod is
+{
+    use crate::
+    {
+        *,
+    };
+
+    //pub fn is_builtin(s: &str) -> bool
+    pub fn api(s: &str) -> bool
+    {
+        unsafe
+            {
+                api::API.contains( &s )
+            }
+    }
+
+    //pub fn is_arithmetic(line: &str) -> bool
+    pub fn arithmetic(line: &str) -> bool
+    {
+        if !regex::contains(line, r"[0-9]+") { return false; }
+
+        if !regex::contains(line, r"\+|\-|\*|/|\^") { return false; }
+
+        regex::contains(line, r"^[ 0-9\.\(\)\+\-\*/\^]+[\.0-9 \)]$")
+    }
+
+    //pub fn is_env(line: &str) -> bool
+    pub fn environment(line: &str) -> bool { regex::contains(line, r"^[a-zA-Z_][a-zA-Z0-9_]*=.*$") }
+
+    //pub fn is_shell_altering_command(line: &str) -> bool {
+    pub fn shell_altering_command(line: &str) -> bool
+    {
+        let line = line.trim();
+
+        if regex::contains(line, r"^[A-Za-z_][A-Za-z0-9_]*=.*$") { return true; }
+
+        line.starts_with("alias ")
+        || line.starts_with("export ")
+        || line.starts_with("unalias ")
+        || line.starts_with("unset ")
+        || line.starts_with("source ")
+    }
+    //pub fn is_signal_handler_enabled() -> bool
+    pub fn signal_handler_enabled() -> bool { env::var("PLS_ENABLE_SIG_HANDLER").is_ok_and(|x| x == "1") }
+}
+
 pub mod lines
 {
     use crate::
     {
+        tokens::{ Tokens },
         *,
     };
 
@@ -32,8 +107,17 @@ pub mod lines
         pub is_complete: bool,
     }
 
-    impl LineInfo {
-        pub fn new(tokens: tokens::Tokens) -> Self {
+    impl LineInfo
+    {
+        pub const fn new() -> Self
+        {
+            Self
+            {
+                tokens: Tokens::new(),
+                is_complete: false,
+            }
+        }
+        pub fn create(tokens: tokens::Tokens) -> Self {
             LineInfo {
                 tokens,
                 is_complete: true,
@@ -47,7 +131,134 @@ pub mod mem
     pub use std::mem::{ * };
 }
 
-pub mod reply
+pub mod now
+{
+    use crate::
+    {
+        *,
+    };
+}
+
+pub mod path
+{
+    pub use std::path::{ * };
+    use crate::
+    {
+        collections::{ HashSet },
+        *,
+    };
+
+    pub const DRIVE:&str = r#"C:"#;
+    pub const OPERATING_SYSTEM:&str = r#"\\WINDOWS"#;
+    pub const SYSTEM:&str = r#"\\SYSTEM32"#;
+
+    pub fn initialize_environment()
+    {
+        unsafe
+        {
+            let mut all_paths: HashSet<PathBuf> = HashSet::new();
+            for x in
+                [
+                    format!( r#"{}{}"#, DRIVE, OPERATING_SYSTEM ),
+                    format!( r#"{}{}{}"#, DRIVE, OPERATING_SYSTEM, SYSTEM ),
+                    format!( r#"{}{}{}\\Drivers"#, DRIVE, OPERATING_SYSTEM, SYSTEM ),
+                    format!( r#"{}{}{}\\Drivers\\DriverData"#, DRIVE, OPERATING_SYSTEM, SYSTEM ),
+                    format!( r#"{}{}{}\\OpenSSH"#, DRIVE, OPERATING_SYSTEM, SYSTEM ),
+                    format!( r#"{}{}{}\\Wbem"#, DRIVE, OPERATING_SYSTEM, SYSTEM ),
+                    format!( r#"{}{}{}\\WindowsPowerShell"#, DRIVE, OPERATING_SYSTEM, SYSTEM ),
+                    format!( r#"{}{}{}\\WindowsPowerShell\\v1.0"#, DRIVE, OPERATING_SYSTEM, SYSTEM ),
+            ]
+            {
+            let path_buf = PathBuf::from(x);
+            if path_buf.exists() {
+            all_paths.insert(path_buf);
+            }
+            }
+
+            if let Ok(env_path) = env::var("PATH") {
+                for one_path in env::split_paths(&env_path) {
+                    if !all_paths.contains(&one_path) {
+                        all_paths.insert(one_path);
+                    }
+                }
+            }
+            let path_var = env::join_paths(all_paths).unwrap_or_default();
+            env::set_var("PATH", path_var);
+        }
+    }
+
+    pub fn is_shell_altering_command(line: &str) -> bool {
+        let line = line.trim();
+        if re_contains(line, r"^[A-Za-z_][A-Za-z0-9_]*=.*$") {
+            return true;
+        }
+        line.starts_with("alias ")
+            || line.starts_with("export ")
+            || line.starts_with("unalias ")
+            || line.starts_with("unset ")
+            || line.starts_with("source ")
+    }
+}
+
+pub mod parses
+{
+    use crate::
+    {
+        *,
+    };
+
+    pub mod line
+    {
+        use crate::
+        {
+            lines::{ LineInfo },
+            *,
+        };
+
+        pub fn parses(line: &str) -> LineInfo
+        {
+            let parsed = LineInfo::new();
+            let mut result = Vec::new();
+
+            if is::arithmetic( line )
+            {
+                for x in line.split(' ')
+                {
+                    result.push((String::from(""), x.to_string()));
+                }
+
+                return LineInfo::create( result );
+            }
+
+            parsed
+        }
+    }
+}
+
+pub mod regex
+{
+    pub use crate::re::{ * };
+    use crate::
+    {
+        *,
+    };
+
+    pub fn contains(text: &str, ptn: &str) -> bool
+    {
+        let re = match Regex::new(ptn)
+        {
+            Ok(x) => x,
+            Err(e) => {
+                println!("Regex new error: {:?}", e);
+                return false;
+            }
+        };
+
+        re.is_match(text)
+    }
+}
+
+pub mod replies
 {
     use crate::{ * };
 
@@ -131,10 +342,11 @@ pub mod system
 /// Parse a command to tokens.
 pub fn parse_line(cmd: &str) -> crate::lines::LineInfo
 {
-    crate::lines::LineInfo::new( cmd )
+    crate::parses::line::parses( cmd )
+    //crate::lines::LineInfo::new( cmd )
 }
 /// Run a command or a pipeline.
-pub fn run(line: &str) -> crate::reply::Reply
+pub fn run( line:&str ) -> crate::replies::Reply
 {
-    crate::reply::Reply::new()
+    crate::replies::Reply::new()
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ pls main binary */
 
 unsafe fn domain()
 {
-
+    /*
+    libc::signal(libc::SIGPIPE, libc::SIG_DFL);
+    // ignore SIGTSTP (ctrl-Z) for the shell itself
+    libc::signal(libc::SIGTSTP, libc::SIG_IGN);
+    libc::signal(libc::SIGQUIT, libc::SIG_IGN);
+    */
 }
 
 fn main()
